d5115df12de5a55089407451fa60ab78dcaf83c7
Setsuna Yukiii <siang-meng@live.com>
2023-05-03 17:31:07 +0800

    shortened generated ID

--- backend/helperFunctions/urlManager.js
+++ backend/helperFunctions/urlManager.js
@@ -1,8 +1,12 @@
 // shorten URL
 const short = require('short-uuid');
 const generateShortenedID = () => {
-    const id = short.generate();
-    return id;
+    let id = short.generate();
+    // 22-6 = 16
+    // rng from 0 to 15
+    const randomPart = Math.floor(Math.random() * 16)
+    const generatedId = id.slice(randomPart,randomPart+6);
+    return generatedId;
 };
 
 module.exports = { generateShortenedID };
