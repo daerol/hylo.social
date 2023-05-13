d5b1be94fdc875314b8ad9ef75490257ddff94cd
Setsuna Yukiii <siang-meng@live.com>
2023-05-13 18:30:05 +0800

    Swagger done for users

--- backend/Routes/UserRoutes.js
+++ backend/Routes/UserRoutes.js
@@ -18,13 +18,14 @@ const upload = multer({
 
 // ===========================all routes===========================
 // =========================Create=========================
-
-
 /**
  * @swagger
- * /user/login: 
+ * /users/login: 
  *   post:
  *     summary: Login user.
+ *     tags:
+ *       - users
+ *     description: Login user using email and password
  *     requestBody:
  *       required: true
  *       content:
@@ -41,24 +42,315 @@ const upload = multer({
  *                 description: The user's password.
  *                 example: password
  *     responses:
- *       201:
- *         ...
+ *       200:
+ *         description: Login successful
+ *       404:
+ *         description: User not found
+ *       400:
+ *         description: Invalid Password
+ *       500:
+ *         description: Something went wrong
  */ 
-
 router.post("/login", userController.loginUser);
+
+/**
+ * @swagger
+ * /users/: 
+ *   post:
+ *     summary: Register user.
+ *     tags:
+ *       - users
+ *     description: Creates new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: The user's usernmae.
+ *                 example: testusername
+ *               email:
+ *                 type: string
+ *                 description: The user's email.
+ *                 example: example@email.com
+ *               password:
+ *                 type: string
+ *                 description: The user's password.
+ *                 example: password
+ *               file:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       200:
+ *         description: User successfully created
+ *       400:
+ *         description: Invalid/taken email/username, password less than 6 characters
+ *       500:
+ *         description: Something went wrong
+ */ 
 router.post("/", upload.single("file"), userController.createUser);
 // =========================Read=========================
+/**
+ * @swagger
+ * /users/: 
+ *  get:
+ *      summary: This endpoint is to allow us to check all users :)
+ *      tags:
+ *        - users
+ *        - test
+ *      description: Get all users in database
+ *      responses:
+ *          200:
+ *              description: To test Get method 
+ *          500:
+ *              description: Something went wrong
+ */
 router.get("/", userController.findAllUsers);
+
+/**
+ * @swagger
+ * /users/db/{userId}: 
+ *  get:
+ *      summary: This endpoint allows us to retrive user by their database generated ID
+ *      tags:
+ *        - users
+ *      description: Get user with given database-generated ID as the parameter
+ *      parameters:
+ *          - in: path
+ *            name: userId
+ *            required: true
+ *            description: User ID (from database)
+ *            schema:
+ *              type: string
+ *      responses:
+ *          200:
+ *              description: User found
+ *          404:
+ *              description: User not found
+ *          500:
+ *              description: Something went wrong
+ */
 router.get("/db/:userId", userController.findUserByDbId);
+
+/**
+ * @swagger
+ * /users/id/{genId}: 
+ *  get:
+ *      summary: This endpoint allows us to retrive user by their unique generated ID (can be regenerated)
+ *      tags:
+ *        - users
+ *      description: Get user with unique generated ID (can be regenerated) as the parameter
+ *      parameters:
+ *          - in: path
+ *            name: genId
+ *            required: true
+ *            description: User unique generated ID (can be regenerated)
+ *            schema:
+ *              type: string
+ *      responses:
+ *          200:
+ *              description: User found
+ *          404:
+ *              description: User not found
+ *          500:
+ *              description: Something went wrong
+ */
 router.get("/id/:genId", userController.findUserByGenId);
+
+/**
+ * @swagger
+ * /users/{username}: 
+ *  get:
+ *      summary: This endpoint allows us to retrive user by their unique username
+ *      tags:
+ *        - users
+ *      description:  Get user with unique username as the parameter
+ *      parameters:
+ *          - in: path
+ *            name: username
+ *            required: true
+ *            description: User's unique username
+ *            schema:
+ *              type: string
+ *      responses:
+ *          200:
+ *              description: User found
+ *          404:
+ *              description: User not found
+ *          500:
+ *              description: Something went wrong
+ */
 router.get("/:username", userController.findUserByUserName);
 // =========================Update=========================
+
+/**
+ * @swagger
+ * /users/{userId}:
+ *  put:
+ *      summary: This allows users to change their unique usernames
+ *      tags:
+ *      - users
+ *      - protected
+ *      description: Takes in new username in the body and userId in the query, which requires JWT authorisation in the request header
+ *      parameters:
+ *          - in: path
+ *            name: userId
+ *            required: true
+ *            description: User database generated ID (cannot be regenerated)
+ *            schema:
+ *              type: string
+ * 
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                      username:
+ *                          type: string
+ *                          description: The user's usernmae.
+ *                          example: testusername
+ *      responses:
+ *          200:
+ *              description: Username successfully updated
+ *          403:
+ *              description: Unauthorised access
+ *          400:
+ *              description: Invalid/taken email/username, password less than 6 characters
+ *          500:
+ *               description: Something went wrong
+ */
 router.put("/:userId", protection, userController.changeUsername);
+
+/**
+ * @swagger
+ * /users/p/{userId}:
+ *  put:
+ *      summary: This allows users to change their profile picture
+ *      tags:
+ *      - users
+ *      description: Replaces profile pic of a user.
+ *      parameters:
+ *          - in: path
+ *            name: userId
+ *            required: true
+ *            description: User unique generated ID (can be regenerated)
+ *            schema:
+ *              type: string
+ * 
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              multipart/form-data:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                      file:
+ *                          description: The new profile pic
+ *                          type: string
+ *                          format: binary
+ *
+ *      responses:
+ *          200:
+ *              description: Profile picture successfully updated
+ *          403:
+ *              description: Unauthorised access
+ *          400:
+ *              description: Bad request(no picture)
+ *          500:
+ *               description: Something went wrong
+ */
 router.put("/p/:userId",upload.single("file"),protection,userController.changeProfilePic)
-router.put("/p/d/:userId",protection,userController.removeProfilePic)
+
+/**
+ * @swagger
+ * /users/p/d/{userId}:
+ *  put:
+ *      summary: This allows users to remove their profile picture
+ *      tags:
+ *      - users
+ *      - protected
+ *      description: Removes profile pic of a user.
+ *      parameters:
+ *          - in: path
+ *            name: userId
+ *            required: true
+ *            description: User database generated ID (cannot be regenerated)
+ *            schema:
+ *              type: string
+ *
+ *      responses:
+ *          200:
+ *              description: Profile picture successfully updated
+ *          403:
+ *              description: Unauthorised access
+ *          400:
+ *              description: Bad request
+ *          500:
+ *               description: Something went wrong
+ */
+router.put("/r/:userId",protection,userController.removeProfilePic)
+
+/**
+ * @swagger
+ * /users/r/{userId}:
+ *  put:
+ *      summary: This allows users to regenerate their short code
+ *      tags:
+ *      - users
+ *      - protected
+ *      description: Refreshes shortened ID of user
+ *      parameters:
+ *          - in: path
+ *            name: userId
+ *            required: true
+ *            description: User unique database ID (cannot be regenerated)
+ *            schema:
+ *              type: string
+ *
+ *      responses:
+ *          200:
+ *              description: Profile picture successfully removed
+ *          403:
+ *              description: Unauthorised access
+ *          400:
+ *              description: Bad request
+ *          500:
+ *               description: Something went wrong
+ */
 router.put("/r/:userId", protection, userController.refreshShortenedURL);
 
 // =========================Delete=========================
+
+/**
+ * @swagger
+ * /users/{userId}:
+ *  delete:
+ *      summary: This allows users to delete their accounts
+ *      tags:
+ *      - users
+ *      - protected
+ *      description: Deletes user account with given ID and matching token
+ *      parameters:
+ *          - in: path
+ *            name: userId
+ *            required: true
+ *            description: User unique database ID (cannot be regenerated)
+ *            schema:
+ *              type: string
+ *
+ *      responses:
+ *          200:
+ *              description: User deleted
+ *          403:
+ *              description: Unauthorised access
+ *          500:
+ *              description: Something went wrong
+ */
 router.delete("/:userId", protection, userController.deleteUser);
 // ===========================export routes===========================
 module.exports = router;
--- backend/app.js
+++ backend/app.js
@@ -12,41 +12,50 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 // ======swagger======
 const swaggerJSDoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express")
+const swaggerUi = require("swagger-ui-express");
 const options = {
     explorer: true,
-    definition:{
-        openapi:"3.0.0",
-        info:{
-            title:"Hylo API",
-            version:"1.0.0"
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Hylo API",
+            version: "1.0.0",
         },
-        servers:[
+        servers: [
             {
-                url:'http://localhost:8000'
-            }
-        ]
+                url: "http://localhost:8000",
+            },
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: "http",
+                    scheme: "bearer",
+                    bearerFormat: "JWT",
+                },
+            },
+        },
+        security: [{
+            bearerAuth: []
+        }]
     },
-    apis:[
-        './app.js',
-        './Routes/UserRoutes.js',
-        './Routes/LinkRoutes.js'
-    ]
-}
-const swaggerSpec = swaggerJSDoc(options)
-app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerSpec))
+    apis: ["./app.js", "./Routes/UserRoutes.js", "./Routes/LinkRoutes.js"],
+};
+const swaggerSpec = swaggerJSDoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // ===================Endpoints===================
 // ============default============
 /**
  * @swagger
- * /: 
+ * /:
  *  get:
- *      summary: This API is used as a health check :)
- *      description: This API is used to check if get method is working
+ *      summary: This endpoint is used as a health check :)
+ *      tags:
+ *        - test
+ *      description: This endpoint is used to check if API is working
  *      responses:
  *          200:
  *              description: To test Get method
@@ -57,8 +66,8 @@ app.get("/", (req, res) => {
 });
 
 // ============for user============
-app.use("/users",require("./Routes/UserRoutes"))
+app.use("/users", require("./Routes/UserRoutes"));
 
 // ============for links============
-app.use("/links",require("./Routes/LinkRoutes"))
+app.use("/links", require("./Routes/LinkRoutes"));
 module.exports = app;
